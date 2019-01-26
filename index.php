79abdeaff19489bb518a8432aedcee28df7e5ddc
andresAlas64 <andres.au95@gmail.com>
2019-01-26 13:26:09 -0600

    Cambios en la pagina

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
+<?php
+  $titulo = 'Tecnosoft';
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tecnosoft</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/style.css">
-</head>
+  include_once 'plantillas/docDeclaracion.php';
 
-<body>
+  /*include_once 'plantillas/navbar.php';*/
+?>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="index.html">Tecnosoft</a>
+      <a class="navbar-brand" href="index.php">Tecnosoft</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
@@ -27,7 +20,7 @@
             <a class="nav-link" href="#nosotros">Nosotros</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contáctenos</a>
+            <a class="nav-link" href="contacto.php">Contáctenos</a>
           </li>
         </ul>
       </div>
@@ -140,19 +133,8 @@
       </div>
     </div>
   </section>
-  <!-- Footer -->
-  <footer class="text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 text-white margen-top">
-          <p>© 2019 - Tecnosoft Milenio San José Costa Rica. Todos los derechos reservados.</p>
-        </div>
-      </div>
-    </div>
-  </footer>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-</body>
+  <?php
+    include_once 'plantillas/footer.php';
 
-</html>
+    include_once 'plantillas/docCierre.php';
+  ?>
---
+++ plantillas/footer.php
@@ -0,0 +1,9 @@
+<footer class="text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-white margen-top">
+        <p>© 2019 - Tecnosoft Milenio San José Costa Rica. Todos los derechos reservados.</p>
+      </div>
+    </div>
+  </div>
+</footer>
